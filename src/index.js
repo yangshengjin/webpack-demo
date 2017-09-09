8cba897a1611cd1ff6a21a1406dc39639630a542
YangShengJin <yangshengjinysj@163.com>
2017-09-09 18:31:21 +0800

    study webpack guides output-management

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import _ from 'lodash';
-import './style.css'
-import Logo from './logo.png'
+
+import printMe from './print.js';
 function component() {
   var element = document.createElement('div');
-
-   // Lodash, currently included via a script, is required for this line to work
-   // Lodash, now imported by this script
+  var btn = document.createElement('button');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello', 'bigsize')
-  var myIcon = new Image();
-  myIcon.src = Logo;
-  element.appendChild(myIcon);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn)
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,19 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.(css|less)$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      }  
-    ]
-  }
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ]
 }
