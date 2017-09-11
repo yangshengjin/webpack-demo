06b245b21ea0be3ee51c99614b6218f1cfb4fe81
YangShengJin <yangshengjinysj@163.com>
2017-09-11 18:23:34 +0800

    study guides production

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 
 import { cube } from './math.js'
-
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 function component() {
   var element = document.createElement('pre');
   element.innerHTML = [
     'hello webpack',
-    '5 cubed is equal to ' + cube(5)
+    '4 cubed is equal to ' + cube(4)
   ].join('\n\n');
   return element;
 }
---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = merge(common, {
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+});
